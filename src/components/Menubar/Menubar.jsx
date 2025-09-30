fcbe0c92f7e94d93c4d7fde458be300461946038
Thinuka Vinjaya Wickramanayaka <thinukavinjayauni@gmail.com>
2025-09-30 10:00:57 +0530

    make navbar auth

--- src/components/Menubar/Menubar.jsx
+++ src/components/Menubar/Menubar.jsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { assets } from '../../assets/assets';
 import './Menubar.css';
 import { useContext } from 'react';
@@ -7,19 +7,26 @@ import { AppContext } from '../../context/AppContext'
 const Menubar = () => {
 
     const navigate = useNavigate();
-    const {setAuthData} = useContext(AppContext);
+    const location = useLocation();
+    const { setAuthData, auth } = useContext(AppContext);
     const logout = () => {
-       localStorage.removeItem("token");
-       localStorage.removeItem("role");
-       setAuthData(null, null);
-       navigate("/login");
+        localStorage.removeItem("token");
+        localStorage.removeItem("role");
+        setAuthData(null, null);
+        navigate("/login");
     }
+
+    const isActive = (path) => {
+        return location.pathname === path;
+    }
+
+    const isAdmin = auth.role === "ROLE_ADMIN";
 
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-2">
                 <a className="navbar-brand" href="#">
-                    <img src={assets.computer} alt="Logo" height="40"/>
+                    <img src={assets.computer} alt="Logo" height="40" />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -27,28 +34,34 @@ const Menubar = () => {
                 <div className="collapse navbar-collapse p-2" id="navbarNav">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link " to="/dashbord">Dashbord</Link>
+                            <Link className={`nav-link ${isActive('/dashboard') ? 'fw-bold text-warning' : ''} `} to="/dashbord">Dashbord</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/explore">Explore</Link>
+                            <Link className={`nav-link ${isActive('/explore') ? 'fw-bold text-warning' : ''} `} to="/explore">Explore</Link>
                         </li>
+                        {
+                            isAdmin && (
+                                <>
+                                    <li className="nav-item">
+                                        <Link className={`nav-link ${isActive('/items') ? 'fw-bold text-warning' : ''} `} to="/items">Manage Items</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className={`nav-link ${isActive('/category') ? 'fw-bold text-warning' : ''} `} to="/category">Manage Categories</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className={`nav-link ${isActive('/users') ? 'fw-bold text-warning' : ''} `} to="/users">Manage Users</Link>
+                                    </li>
+                                </>
+                            )
+                        }
                         <li className="nav-item">
-                            <Link className="nav-link" to="/items">Manage Items</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/category">Manage Categories</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/users">Manage Users</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/orders">Order History</Link>
+                            <Link className={`nav-link ${isActive('/orders') ? 'fw-bold text-warning' : ''} `} to="/orders">Order History</Link>
                         </li>
                     </ul>
-                     <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                    <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                         <li className='nav-item dropdown'>
                             <a href='#' className='nav-link dropdown-toggle' id='navbarDropdown' role='button' data-bs-toggle="dropdown" aria-expanded="false">
-                               <img src={assets.profile} alt='' height={32} width={32} /> 
+                                <img src={assets.profile} alt='' height={32} width={32} />
                             </a>
                             <ul className="dropdown-menu dropdown-menu-end" aria-labelledby='navbarDropdown'>
                                 <li>
@@ -59,17 +72,17 @@ const Menubar = () => {
                                         Activity log
                                     </a>
                                     <li>
-                                        <hr className='dropdown-divider'/>
+                                        <hr className='dropdown-divider' />
                                     </li>
                                     <a href='#!' className='dropdown-item' onClick={logout} >
                                         Logout
-                                    </a>                  
+                                    </a>
                                 </li>
                             </ul>
                         </li>
-                     </ul>
-                      
-                </div> 
+                    </ul>
+
+                </div>
             </nav>
         </div>
     )
